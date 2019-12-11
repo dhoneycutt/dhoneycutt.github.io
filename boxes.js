fa627fbe24ee987923c6177467fea2407b9401fe
Donald Honeycutt <dhoneycutt@ufl.edu>
2019-12-11 14:20:44 -0500

    Add box logging

--- boxes.js
+++ boxes.js
@@ -1,27 +1,12 @@
 // JavaScript source code
 images1 = [
     [
-        {
-            "left": "215px",
-            "top": "158px",
-            "width": "63px",
-            "height": "70px"
-        },
-        {
-            "left": "115px",
-            "top": "258px",
-            "width": "63px",
-            "height": "70px"
-        }
+        {"left":"800px","top":"145px","width":"46px","height":"62px"},{"left":"846px","top":"168px","width":"35px","height":"65px"},{"left":"933px","top":"193px","width":"42px","height":"57px"},{"left":"968px","top":"169px","width":"41px","height":"57px"},{"left":"1057px","top":"201px","width":"32px","height":"47px"},{"left":"1072px","top":"146px","width":"39px","height":"61px"}
     ],
+    [],
+    [],
     [
-
-    ],
-    [
-
-    ],
-    [
-
+      {"left":"962px","top":"167px","width":"95px","height":"111px"},{"left":"826px","top":"177px","width":"82px","height":"95px"}
     ],
     [
 
