6e7ef4396824ad8e223008180bf85614e5a27e8b
Donald Honeycutt <dhoneycutt@ufl.edu>
2020-01-07 13:45:52 -0500

    Update images

--- boxes.js
+++ boxes.js
@@ -1,16 +1,7 @@
 // JavaScript source code
 images1 = [
     [
-        {"left":"46.5px","top":"110px","width":"99px","height":"110px"},{"left":"10.5px","top":"319px","width":"32px","height":"44px"}
-    ],
-    [
-        {"left":"139.5px","top":"213px","width":"71px","height":"47px"}
-    ],
-    [
-
-    ],
-    [
-      {"left":"962px","top":"167px","width":"95px","height":"111px"},{"left":"826px","top":"177px","width":"82px","height":"95px"}
+        {"left":"153.60000610351562px","top":"111px","width":"138px","height":"138px"}
     ],
     [
 
@@ -19,77 +10,87 @@ images1 = [
 
     ],
     [
-
+      {"left":"117.60000610351562px","top":"156px","width":"93px","height":"112px"},{"left":"256.6000061035156px","top":"163px","width":"102px","height":"108px"}
     ],
     [
-
+      {"left":"50.600006103515625px","top":"166px","width":"76px","height":"84px"},{"left":"377.6000061035156px","top":"211px","width":"74px","height":"81px"}
     ],
     [
-
+      //5
     ],
     [
-
+      {"left":"168.60000610351562px","top":"100px","width":"133px","height":"158px"},{"left":"324.6000061035156px","top":"207px","width":"41px","height":"49px"},{"left":"9.600006103515625px","top":"176px","width":"36px","height":"56px"}
     ],
     [
-
+      //7
     ],
     [
-
+      {"left":"71.60000610351562px","top":"106px","width":"28px","height":"38px"},{"left":"153.60000610351562px","top":"83px","width":"31px","height":"33px"},{"left":"227.60000610351562px","top":"82px","width":"39px","height":"37px"},{"left":"386.6000061035156px","top":"85px","width":"27px","height":"31px"}
     ],
     [
-
+      {"left":"154.60000610351562px","top":"139px","width":"59px","height":"76px"},{"left":"296.6000061035156px","top":"115px","width":"54px","height":"72px"}
     ],
     [
-
+      //10
     ],
     [
-
+      {"left":"140.60000610351562px","top":"109px","width":"64px","height":"121px"},{"left":"365.6000061035156px","top":"130px","width":"69px","height":"112px"}
     ],
     [
-
+      {"left":"198.60000610351562px","top":"86px","width":"91px","height":"128px"}
     ],
     [
-
+      //13
     ],
     [
-
+      //14
     ],
     [
-
+      //15
     ],
     [
-
+      {"left":"201.60000610351562px","top":"159px","width":"19px","height":"37px"},{"left":"269.6000061035156px","top":"178px","width":"23px","height":"34px"},{"left":"340.6000061035156px","top":"166px","width":"29px","height":"41px"}
     ],
     [
-
+      {"left":"102.60000610351562px","top":"172px","width":"54px","height":"57px"}
     ],
     [
-
+      {"left":"355.6000061035156px","top":"309px","width":"47px","height":"50px"}
     ],
     [
-
+      {"left":"59.600006103515625px","top":"138px","width":"35px","height":"41px"},{"left":"112.60000610351562px","top":"147px","width":"25px","height":"30px"},{"left":"156.60000610351562px","top":"152px","width":"19px","height":"30px"},{"left":"196.60000610351562px","top":"151px","width":"21px","height":"33px"},{"left":"239.60000610351562px","top":"148px","width":"29px","height":"34px"},{"left":"282.6000061035156px","top":"142px","width":"25px","height":"38px"},{"left":"329.6000061035156px","top":"148px","width":"37px","height":"32px"},{"left":"385.6000061035156px","top":"142px","width":"33px","height":"41px"}
     ],
     [
-
+      //20
     ],
     [
-
+      {"left":"193.60000610351562px","top":"196px","width":"53px","height":"78px"}
     ],
     [
-
+      {"left":"298.6000061035156px","top":"208px","width":"44px","height":"66px"},{"left":"34.600006103515625px","top":"114px","width":"149px","height":"184px"},{"left":"357.6000061035156px","top":"176px","width":"88px","height":"123px"}
     ],
     [
-
+      {"left":"117.60000610351562px","top":"167px","width":"40px","height":"57px"},{"left":"219.60000610351562px","top":"139px","width":"48px","height":"55px"},{"left":"348.6000061035156px","top":"134px","width":"47px","height":"59px"}
     ],
     [
-
+      //24
     ],
     [
-
+      {"left":"256.6000061035156px","top":"149px","width":"85px","height":"95px"},{"left":"369.6000061035156px","top":"263px","width":"49px","height":"74px"}
     ],
     [
-
+      {"left":"356.6000061035156px","top":"274px","width":"41px","height":"54px"},{"left":"331.6000061035156px","top":"216px","width":"41px","height":"55px"},{"left":"157.60000610351562px","top":"167px","width":"50px","height":"63px"}
+    ],
+    [
+      {"left":"63.600006103515625px","top":"206px","width":"30px","height":"44px"}
+    ],
+    [
+      {"left":"133.60000610351562px","top":"157px","width":"59px","height":"111px"},{"left":"227.60000610351562px","top":"183px","width":"90px","height":"77px"},{"left":"361.6000061035156px","top":"156px","width":"72px","height":"106px"}
+    ],
+    [
+      {"left":"161.60000610351562px","top":"115px","width":"56px","height":"87px"},{"left":"265.6000061035156px","top":"122px","width":"62px","height":"71px"}
     ]
+
 ]
 
     images2 = [],
